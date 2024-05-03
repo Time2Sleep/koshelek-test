5e2228d4da6aed801b3591f8b7f0ca7b15c4e398
Time2Sleep <omg.airat@gmail.com>
2024-05-03 19:45:12 +0300

    Updates

---
+++ assets/HomeView-DQMpipbd.js
@@ -0,0 +1,1 @@
+import{d as e,c as o,o as a}from"./index-CWfLvPEv.js";const s=e({__name:"HomeView",setup(n){return console.log("ahaha"),(t,c)=>(a(),o("h1",null,"Тестовое задание"))}});export{s as default};
